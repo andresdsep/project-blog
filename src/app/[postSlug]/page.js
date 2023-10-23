c6d075dd2594fc605d3d9ed53182c1d3fa9ac0b8
Andres Sepulveda <a.sepulveda@valdperformance.com>
2023-10-23 14:34:55 +1000

    feat: Exercise 5A solution

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,8 +1,14 @@
 import BlogHero from '@/components/BlogHero';
 import { MDXRemote } from 'next-mdx-remote/rsc';
+import dynamic from 'next/dynamic';
+import CodeSnippet from '../../components/CodeSnippet';
+import Spinner from '../../components/Spinner';
 import { loadBlogPost } from '../../helpers/file-helpers';
 import styles from './postSlug.module.css';
-import CodeSnippet from '../../components/CodeSnippet';
+
+const DivisionGroupsDemo = dynamic(() => import('../../components/DivisionGroupsDemo'), {
+  loading: Spinner,
+});
 
 export async function generateMetadata({ params: { postSlug } }) {
   const {
@@ -24,7 +30,7 @@ async function BlogPost({ params: { postSlug } }) {
     <article className={styles.wrapper}>
       <BlogHero title={title} publishedOn={publishedOn} />
       <div className={styles.page}>
-        <MDXRemote source={content} components={{pre: CodeSnippet}} />
+        <MDXRemote source={content} components={{ pre: CodeSnippet, DivisionGroupsDemo }} />
       </div>
     </article>
   );
